645b2069adf65a3d26ccebb3676989eada0167c4
scnbwa <boyce.wang@sas.com>
2016-08-02 15:51:58 +0800

    add avatar in the profile page

--- profile/profile.php
+++ profile/profile.php
@@ -8,6 +8,8 @@
             echo json_encode(getProfile(1));
         } else if ($type == "basic") {
             echo json_encode(getBasicInfo(1));
+        } else if ($type == "avatar") {
+            echo getAvatar(1);
         }
     }
     $dba->disconnect();
@@ -21,6 +23,14 @@
     function getBasicInfo ($baby_id) {
         global $dba;
         $result = $dba->query("SELECT name, birthday FROM baby WHERE id = ". $baby_id . ";");
+        return $result[0];
+    }
+
+    function getAvatar ($baby_id) {
+        global $dba;
+        $result = $dba->query("SELECT avatar from baby WHERE id = ". $baby_id . ";", function($row){
+            return $row["avatar"];
+        });
         return $result[0];
     }
 ?>
