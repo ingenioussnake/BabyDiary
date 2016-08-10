6bc03a5769ab5c5a101ca9fcd157cba798fa1a84
Boyce <ingenioussnake@sina.com>
2016-08-10 23:37:30 +0800

    support height / weight incremental statistic and bmi

--- db/statistic.php
+++ db/statistic.php
@@ -84,17 +84,31 @@
         $unit_cal = getUnitCalculation($unit, $type, true);
         $stmt = "select ".$unit_cal." as ".$unit.",  MAX(".$type.") as ".$type." from ".$type." where baby = 1 group by ".$unit." order by ".$unit.";";
         $result = $dba->query($stmt);
-        if ($_GET["flag"] == "true") {
-            //
+        if ($_GET["flag"] == "true") { //incremental
+            for($i = count($result) - 1; $i > 0; $i--) {
+                $curr = $result[$i];
+                $prev = $result[$i-1];
+                if ($curr[$unit] - $prev[$unit] == 1) {
+                    $result[$i][$type] = $curr[$type] - $prev[$type];
+                } else {
+                    array_splice($result, $i, 1);
+                }
+            }
+            $result[0][$type] = 0;
         }
         return json_encode($result);
     }
 
     function getCompGrowth () {
         global $dba;
-        $stmt = "select weight.weight as weight,  height.height as height from height, weight where height.baby = 1 and weight.baby = 1 and height.date = weight.date order by height asc";
-        $result = $dba->query($stmt);
-        return json_encode($result);
+        if ($_GET["flag"] === "true") { // BMI
+            // $unit = $_GET["unit"];
+            // $unit_cal = getUnitCalculation($unit, $type, true);
+            $stmt = "select ht.week, wt.weight / (ht.height * ht.height * 0.0001) as bmi from (select ceiling(datediff(date, (select birthday from baby where height.baby = id)) / 7) as week, Max(height) as height from height where baby = 1 group by week) as ht inner join (select ceiling(datediff(weight.date, (select birthday from baby where weight.baby = id)) / 7) as week, Max(weight) as weight from weight where baby = 1 group by week) as wt on ht.week = wt.week";
+        } else {
+            $stmt = "select weight.weight as weight,  height.height as height from height, weight where height.baby = 1 and weight.baby = 1 and height.date = weight.date order by height asc";
+        }
+        return json_encode($dba->query($stmt));
     }
 
     function getUnitCalculation ($unit, $table, $since = false) {
